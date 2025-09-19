bd8beda8b502a48b40714cf42b45520eb91f8e4b
José Ronaldo <107088503+jronaldobs@users.noreply.github.com>
2025-09-19 16:35:21 -0400

    supabasee

--- src/app/lib/supabase.js
+++ src/app/lib/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Certifique-se de que estas variáveis de ambiente estão definidas no seu arquivo .env.local
-const supabaseUrl = https://vfusutiopoizmlwmevum.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdXN1dGlvcG9pem1sd21ldnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjQ3NDIsImV4cCI6MjA3MzgwMDc0Mn0.SfQmRBJwVuuEyGKUlBTFa3PlDIcB80B6LojigE7F9zk
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_vfusutiopoizmlwmevum.supabase.co
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdXN1dGlvcG9pem1sd21ldnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjQ3NDIsImV4cCI6MjA3MzgwMDc0Mn0.SfQmRBJwVuuEyGKUlBTFa3PlDIcB80B6LojigE7F9zk
 
 // Cria uma única instância do cliente Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
