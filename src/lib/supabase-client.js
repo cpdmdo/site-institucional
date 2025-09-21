6a8e3dc287371c8ddbe9ac97d16daa586ad88c1d
Jose Ronaldo <ronaldomirassol@gmail.com>
2025-09-21 18:09:37 -0400

    ajuste supabaseajuste2

--- src/lib/supabase-client.js
+++ src/lib/supabase-client.js
@@ -1,10 +1,11 @@
-// lib/supabase.js
-import { createClient } from '@supabase/supabase-js'
+// src/lib/supabase-client.js
 
-// Certifique-se de que estas variáveis de ambiente estão definidas no seu arquivo .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_vfusutiopoizmlwmevum.supabase.co
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdXN1dGlvcG9pem1sd21ldnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjQ3NDIsImV4cCI6MjA3MzgwMDc0Mn0.SfQmRBJwVuuEyGKUlBTFa3PlDIcB80B6LojigE7F9zk
+// Importa a função de criação de cliente do Supabase
+import { createClient } from '@supabase/supabase-js';
 
-// Cria uma única instância do cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-//testes.
+// Define as variáveis de ambiente. Elas devem ter o prefixo NEXT_PUBLIC_
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Cria a instância do cliente Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
